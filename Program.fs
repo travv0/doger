f0d4f9f18a9963e168b5c930e4d64a67de9d61c8
Travis <tssund93@gmail.com>
2021-03-04 17:20:02 -0600

    doge collide

--- Program.fs
+++ Program.fs
@@ -7,27 +7,46 @@ let tileSize = 32f
 let (windowWidth, windowHeight) = (640, 480)
 
 [<AbstractClass>]
-type Object(x: float32, y: float32, sprite: Image) =
+type GameObject(x: float32, y: float32, sprite: Image) =
     inherit Scene()
     member val X = x with get, set
     member val Y = y with get, set
+    member __.Width = sprite.GetWidth() |> float32
+    member __.Height = sprite.GetHeight() |> float32
     member __.Sprite = sprite
 
     default this.Draw() =
         Graphics.Draw(this.Sprite, this.X, this.Y, 0f, 1f, 1f, 0f, 0f)
 
+    member object.Intersects(otherObject: GameObject) =
+        [ {| X = object.X; Y = object.Y |}
+          {| X = object.X + object.Width - 1f
+             Y = object.Y |}
+          {| X = object.X + object.Width - 1f
+             Y = object.Y + object.Height - 1f |}
+          {| X = object.X
+             Y = object.Y + object.Height - 1f |} ]
+        |> Seq.exists
+            (fun corner ->
+                corner.X >= otherObject.X
+                && corner.X < otherObject.X + otherObject.Width
+                && corner.Y >= otherObject.Y
+                && corner.Y < otherObject.Y + otherObject.Height)
+
+    abstract member Collide : GameObject -> unit
+    default __.Collide(_) = ()
+
 type HorizontalMover(x, y, moveSpeed, sprite) =
-    inherit Object(x, y, sprite)
+    inherit GameObject(x, y, sprite)
 
     override object.Update(dt) =
         object.X <- object.X + moveSpeed * dt
-        let objectWidth = object.Sprite.GetWidth() |> float32
 
-        if object.X < -objectWidth then
-            object.X <- float32 windowWidth + objectWidth
+        if object.X < -object.Width then
+            object.X <- float32 windowWidth + object.Width
 
-        if object.X > float32 windowWidth + objectWidth then
-            object.X <- -objectWidth |> float32
+        if object.X > float32 windowWidth + object.Width then
+            object.X <- -object.Width
 
 type Car(x, y, moveSpeed) =
     inherit HorizontalMover(x, y, moveSpeed, Graphics.NewImage("media/sprites/doge.png"))
@@ -36,9 +55,9 @@ type Log(x, y, moveSpeed) =
     inherit HorizontalMover(x, y, moveSpeed, Graphics.NewImage("media/sprites/doge.png"))
 
 type Doge() =
-    inherit Object(float32 windowWidth / 2f,
-                   float32 windowHeight - tileSize,
-                   Graphics.NewImage("media/sprites/doge.png"))
+    inherit GameObject(float32 windowWidth / 2f,
+                       float32 windowHeight - tileSize,
+                       Graphics.NewImage("media/sprites/doge.png"))
 
     override doge.KeyPressed(key, _, _) =
         match key with
@@ -48,12 +67,14 @@ type Doge() =
         | KeyConstant.Down -> doge.Y <- doge.Y + tileSize
         | _ -> ()
 
+    override doge.Collide(otherObject) = printfn "doge collide"
+
 type Playing() =
     inherit Scene()
 
     let doge = Doge()
 
-    let objects : Object list =
+    let objects : GameObject list =
         [ doge
           Car(0f, 0f, 100f)
           Car(0f, 32f, -200f)
@@ -74,6 +95,14 @@ type Playing() =
     override __.Update(dt) =
         for object in objects do
             object.Update(dt)
+
+        for object in objects do
+            for otherObject in objects do
+                if
+                    object <> otherObject
+                    && object.Intersects(otherObject)
+                then
+                    object.Collide(otherObject)
 
 type Program() =
     inherit Scene()
