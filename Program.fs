7d4a7ba9e2401f7b5392d6245604a20dacf6858b
Travis <tssund93@gmail.com>
2021-03-04 14:48:47 -0600

    add car class and restructure things a bit

--- Program.fs
+++ Program.fs
@@ -6,37 +6,51 @@ let tileSize = 32f
 
 let (windowWidth, windowHeight) = (640, 480)
 
-type Doge() =
+[<AbstractClass>]
+type Object(x: float32, y: float32, sprite: Drawable) =
     inherit Scene()
+    member val X = x with get, set
+    member val Y = y with get, set
+    member __.Sprite = sprite
 
-    let mutable x, y =
-        float32 windowWidth / 2f, float32 windowHeight - tileSize
+    default this.Draw() =
+        Graphics.Draw(this.Sprite, this.X, this.Y, 0f, 1f, 1f, 0f, 0f)
 
-    let image =
-        Graphics.NewImage("media/sprites/doge.png")
+type Car(x, y, moveSpeed) =
+    inherit Object(x, y, Graphics.NewImage("media/sprites/doge.png"))
 
-    let moveSpeed = 32
+    override car.Update(dt) = car.X <- car.X + moveSpeed * dt
 
-    override __.Draw() =
-        Graphics.Draw(image, x, y, 0f, 1f, 1f, 0f, 0f)
+type Doge() =
+    inherit Object(float32 windowWidth / 2f,
+                   float32 windowHeight - tileSize,
+                   Graphics.NewImage("media/sprites/doge.png"))
 
-    override __.KeyPressed(key, _, _) =
+    override doge.KeyPressed(key, _, _) =
         match key with
-        | KeyConstant.Right -> x <- x + tileSize
-        | KeyConstant.Left -> x <- x - tileSize
-        | KeyConstant.Up -> y <- y - tileSize
-        | KeyConstant.Down -> y <- y + tileSize
+        | KeyConstant.Right -> doge.X <- doge.X + tileSize
+        | KeyConstant.Left -> doge.X <- doge.X - tileSize
+        | KeyConstant.Up -> doge.Y <- doge.Y - tileSize
+        | KeyConstant.Down -> doge.Y <- doge.Y + tileSize
         | _ -> ()
 
 type Playing() =
     inherit Scene()
 
     let doge = Doge()
+    let objects : Object list = [ doge; Car(0f, 0f, 40f) ]
 
-    override __.Draw() = doge.Draw()
+    override __.Draw() =
+        for object in objects do
+            object.Draw()
 
     override __.KeyPressed(key, scancode, isRepeat) =
-        doge.KeyPressed(key, scancode, isRepeat)
+        for object in objects do
+            object.KeyPressed(key, scancode, isRepeat)
+
+    override __.Update(dt) =
+        for object in objects do
+            object.Update(dt)
 
 type Program() =
     inherit Scene()
@@ -44,6 +58,8 @@ type Program() =
     let mutable state = Playing() :> Scene
 
     override __.Draw() = state.Draw()
+
+    override __.Update(dt) = state.Update(dt)
 
     override __.KeyPressed(key, scancode, isRepeat) =
         state.KeyPressed(key, scancode, isRepeat)
