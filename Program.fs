69e4db97a7380bc720a0544f0e501e0e1e52292c
Travis <tssund93@gmail.com>
2021-03-03 20:32:52 -0600

    first commit

---
+++ Program.fs
@@ -0,0 +1,54 @@
+module Main
+
+open Love
+
+let tileSize = 32f
+
+let (windowWidth, windowHeight) = (640, 480)
+
+type Doge() =
+    inherit Scene()
+
+    let mutable x, y =
+        (float32 windowWidth / 2f, float32 windowHeight - tileSize)
+
+    let image = Graphics.NewImage("doge.png")
+    let moveSpeed = 32
+
+    override __.Draw() =
+        Graphics.Draw(image, x, y, 0f, 1f, 1f, 0f, 0f)
+
+    override __.KeyPressed(key, _, isRepeat) =
+        match key with
+        | KeyConstant.Right -> x <- x + tileSize
+        | KeyConstant.Left -> x <- x - tileSize
+        | KeyConstant.Up -> y <- y - tileSize
+        | KeyConstant.Down -> y <- y + tileSize
+        | _ -> ()
+
+type Playing() =
+    inherit Scene()
+
+    let doge = Doge()
+
+    override __.Draw() = doge.Draw()
+
+    override __.KeyPressed(key, scancode, isRepeat) =
+        doge.KeyPressed(key, scancode, isRepeat)
+
+type Program() =
+    inherit Scene()
+
+    let mutable state = Playing() :> Scene
+
+    override __.Draw() = state.Draw()
+
+    override __.KeyPressed(key, scancode, isRepeat) =
+        state.KeyPressed(key, scancode, isRepeat)
+
+[<EntryPoint>]
+let main _ =
+    Boot.Init(BootConfig(WindowTitle = "Doger", WindowWidth = windowWidth, WindowHeight = windowHeight))
+    Boot.Run(Program())
+
+    0
